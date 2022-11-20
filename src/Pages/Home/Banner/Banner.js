a52d457e8122dd425dcb80d2537f17f0bad1add7
Showrove Roy <showrove200@gmail.com>
2022-11-20 15:22:53 +0600

    Banner responsive done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,15 +6,17 @@ import bg from "../../../assets/images/bg.png";
 const Banner = () => {
   return (
     <div
-      className='px-20 py-32'
+      className='lg:px-20 py-5 lg:py-32'
       style={{
         background: `url(${bg}) no-repeat center center / cover`,
       }}>
       <div className='hero'>
         <div className='hero-content flex-col lg:flex-row-reverse'>
-          <img src={chair} className='w-1/2 rounded-lg shadow-2xl' alt='' />
-          <div className='w-1/2'>
-            <h1 className='text-5xl font-bold'>Your New Smile Starts Here</h1>
+          <img src={chair} className='md:w-1/2 rounded-lg shadow-2xl' alt='' />
+          <div className='md:w-1/2'>
+            <h1 className='md:text-5xl text-3xl font-bold'>
+              Your New Smile Starts Here
+            </h1>
             <p className='py-6 text-muteText'>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
